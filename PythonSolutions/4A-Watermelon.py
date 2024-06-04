42210df123703b6837e1ff5b292d79c710f7dd30
Elif <145778497+mavisellien@users.noreply.github.com>
2024-06-04 20:19:57 +0800

    Create 4A-Watermelon.py

---
+++ PythonSolutions/4A-Watermelon.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    weight = int(input(""))
+    def divisible(w):
+        if w%2==0 and w!=2:
+            print("YES")
+        else:
+            print("NO")
+    divisible(weight)
